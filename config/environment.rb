c3eb13ebfdafd4a57fb95f5d06c4c3163b77f413
SRJ007 <suraj.yadav1994@gmail.com>
2017-12-12 04:41:25 +0000

    fixed typo issues in environment.rb file (fixed production issue)

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
-    :user_name => ENV["SENDGRID_USERANME"],
+    :user_name => ENV["SENDGRID_USERNAME"],
     :password => ENV["SENDGRID_PASSWORD"],
-    :domain => 'heoku.com',
+    :domain => 'heroku.com',
     :enable_startrls_auto => true
 }
