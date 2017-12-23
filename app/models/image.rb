b5e4375b5291e358d63d975163d10ad8aea53e53
SRJ007 <suraj.yadav1994@gmail.com>
2017-12-23 06:04:12 +0000

    added file upload feature

---
+++ app/models/image.rb
@@ -0,0 +1,19 @@
+class Image < ActiveRecord::Base
+  belongs_to :user
+  mount_uploader :picture, AvatarUploader
+  
+  include CarrierWave::MiniMagick
+  
+  #TODO need to work in process method which belongs to image magick gem
+  #process resize_to_limit: [300, 300]
+
+  validate :image_size
+  
+  private
+  
+  def image_size
+      if image.picture.size > 5.megabytes
+          self.errors.add(:picture,"size cannot be greter than 5 MB")
+      end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :payment_methods
+  has_many :images
   # accepts_nested_attributes_for helps parent model to save data of its associated tables or models
   # user has one to many association with payment_methods and by allowing accepts nested attributes we can 
   # make user to dave data for payment methods
