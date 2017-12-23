a48afae9059196c49e3a258938fd96f9b4e8fdb4
SRJ007 <suraj.yadav1994@gmail.com>
2017-12-23 13:50:26 +0000

    added environment based check for fog to access AWS

--- config/initializers/carrirer_wave_aws_config.rb
+++ config/initializers/carrirer_wave_aws_config.rb
@@ -1,10 +1,14 @@
-if Rails.env.production?
+
     CarrierWave.configure do |config|
-        config.fog_credentials = {
-            :provider => "AWS",
-            :aws_access_key_id => ENV["AWS_ACCESS_KEY"],
-            :aws_secret_access_key => ENV["AWS_SECRET_KEY"]
-        }
-        config.fog_directory = ENV["S3_BUCKET"]
+        if Rails.env.production?
+            config.fog_credentials = {
+                :provider => "AWS",
+                :aws_access_key_id => ENV["AWS_ACCESS_KEY"],
+                :aws_secret_access_key => ENV["AWS_SECRET_KEY"]
+            }
+            config.fog_directory = ENV["S3_BUCKET"]
+            config.storage = :fog
+        else
+            config.storage = :file
+        end
     end 
-end
